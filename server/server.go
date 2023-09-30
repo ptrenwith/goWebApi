60caf3548a73baf37d3628b47ba31c1b6ac95688
Philip Trenwith <ptrenwith@gmail.com>
2023-09-30 14:17:03 +0200

    Removed old code

--- server/server.go
+++ server/server.go
@@ -73,30 +73,3 @@ func StartGRPCGateway() {
 	log.Println("Serving gRPC-Gateway on", gwServer.Addr)
 	log.Fatalln(gwServer.ListenAndServe())
 }
-
-/*
-func ConnectAndExchange(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	test_id := vars["test_id"]
-	log.Println("Test id request from user : ", test_id)
-
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(grpcserver, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
-
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	resp, err := c.SayHello(ctx, &pb.HelloRequest{Name: test_id})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting: %s", resp.GetMessage())
-
-	w.Write([]byte(resp.GetMessage()))
-}
-*/
